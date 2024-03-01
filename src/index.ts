6c8ee3edbd5602503fcde4b829ce92679cf5dbb4
Sushant Rajbanshi <sushant.rajbanshi@webo.digital>
2024-03-01 16:45:41 +0545

    feat: Write control_flow func from NodeJS Doc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-function test(): void {
-  console.log(process.env.TEST_VALUE);
-  console.error("THIS IS A TEST");
+// function test(): void {
+//   console.log(process.env.TEST_VALUE);
+//   console.error("THIS IS A TEST");
+// }
+
+// test();
+
+
+function getSong() {
+  let _song = '';
+  let i = 100;
+  for (i; i > 0; i -= 1) {
+    _song += `${i} beers on the wall, you take one down and pass it around, ${
+      i - 1
+    } bottles of beer on the wall\n`;
+    if (i === 1) {
+      _song += "Hey let's get some more beer";
+    }
+  }
+
+  return _song;
 }
 
-test();
+function getSongError() {
+  let _song = '';
+  let i = 100;
+  for (i; i > 0; i -= 1) {
+    /* eslint-disable no-loop-func */
+    setTimeout(function () {
+      _song += `${i} beers on the wall, you take one down and pass it around, ${
+        i - 1
+      } bottles of beer on the wall\n`;
+      if (i === 1) {
+        _song += "Hey let's get some more beer";
+      }
+    }, 0);
+    /* eslint-enable no-loop-func */
+  }
+
+  return _song;
+}
+
+function singSong(_song: string) {
+  if (!_song) throw new Error("song is '' empty, FEED ME A SONG!");
+  console.log(_song);
+}
+
+const song = getSong();
+const songError = getSongError();
+// this will work
+// singSong(song);
+singSong(songError);
+
